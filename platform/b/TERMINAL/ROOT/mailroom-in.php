aa4f59924b8a5ed5815b7521c37342e7082e50f2
Dani 'Kat' Leve <the.danibobby@gmail.com>
2026-04-07 07:36:40 -0400

    Rosewood8  D7:T36:N40 on EPO7 GAIA R2026 ED04:ET2:EN07 >| COMMIT 4 >| Leaf: SMALL header refinements, nothing smashing, but quite cute
    Affected files:
    platform/b/TERMINAL/ROOT/mail-viewer.php
    platform/b/TERMINAL/ROOT/mailroom-in.php
    platform/b/TERMINAL/ROOT/mailroom-out.php
    platform/b/TERMINAL/ROOT/mailroom-send.php

--- platform/b/TERMINAL/ROOT/mailroom-in.php
+++ platform/b/TERMINAL/ROOT/mailroom-in.php
@@ -2,9 +2,11 @@
 openSky("TERMINAL.ROOT");
 nameSelf("ROOT");
 section($right,'color:gray','');
-bigHeading("TERMINAL.ROOT COMMUNITS");
-leaf("the public considerations of the terminal at its root.
+
+bigHeading("MAILROOM INBOX");
+leaf("INGOING INTERA MAIL CHARLIE OMEGA 4.3.3
 $BR  .........");
+
 close_section();
 getTool('mailroomBasic', 'ViewInbox');
 
--- platform/b/TERMINAL/ROOT/mailroom-out.php
+++ platform/b/TERMINAL/ROOT/mailroom-out.php
@@ -2,9 +2,11 @@
 openSky("TERMINAL.ROOT");
 nameSelf("ROOT");
 section($right,'color:gray','');
+
 bigHeading("MAILROOM OUTBOX");
-leaf("OUTCOMING MAIL CHARLIE OMEGA 4.3.3
+leaf("OUTCOMING INTERA MAIL CHARLIE OMEGA 4.3.3
 $BR  .........");
+
 close_section();
 
 medHeading("Sorted oldest to newest.");
